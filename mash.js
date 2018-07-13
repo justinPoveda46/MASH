aafc8102dc75543e6c9481016e87c9c78b147cf8
justin.poveda@uasdc.org <justin.poveda@uasdc.org>
2018-07-13 15:35:32 -0700

    mashGame

---
+++ mash.js
@@ -0,0 +1,70 @@
+function mash() {
+return getChildrenCount();
+return getCar();
+}
+console.log(mash());
+
+function getHome(){
+ 
+
+let homes =  ["Mansion", "Apartment", "Shack", "House",];
+homes.push(process.argv[2]);
+homes.push("tunnel")
+return homes[Math.floor(Math.random()* homes.length)]
+
+}
+console.log(getHome());
+
+function getChildrenCount() {
+   let array = [ Math.floor(Math.random()*100+ 1), process.argv[3]]
+   let chance = Math.floor(Math.random()*2 )
+return "You will live in a " + getHome() + ", and you will have " + array[chance]  + " kids!" + "and youl'll drive a " + getCar();
+
+}
+
+console.log(getChildrenCount());
+
+function getCar(){
+
+let vechiles = ["rari","wagon","lambo","a rock"];
+return vechiles[Math.floor(Math.random()* vechiles.length)]
+
+}
+ console.log(getCar());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function mash() {
+//     return getChildrenCount();
+    
+//     }
+//     console.log(mash());
+    
+//     function getHome(){
+     
+//     let homes =  ["Mansion", "Apartment", "Shack", "House"];
+    
+//     return homes[Math.floor(Math.random()* homes.length +1)]
+//     }
+//     console.log(getHome());
+    
+//     function getChildrenCount() {
+    
+//     return "You will live in a " + getHome() + ", and you will have " + Math.floor(Math.random()*100+ 1) + " kids!"
+    
+//     }
+    
+//     console.log(getChildrenCount());
+
+
